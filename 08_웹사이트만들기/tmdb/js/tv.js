549718e28d66c001604dd252aedfe586c9183571
min239 <gksckdalsmark@naver.com>
2025-05-30 01:40:44 +0900

    first commit

--- 08_웹사이트만들기/tmdb/js/tv.js
+++ 08_웹사이트만들기/tmdb/js/tv.js
@@ -6,46 +6,53 @@ const options = {
    },
 }
 
-const tvPopularUrl = 'https://api.themoviedb.org/3/tv/popular?language=ko-KR&page=1'
+const url = 'https://api.themoviedb.org/3/discover/tv?language=ko-KR&page=3&with_original_language=ko'
 
-const getPlayingTv = async (tvPopularUrl) => {
+const getPlayingtvs = async (url) => {
    try {
-      const res = await fetch(tvPopularUrl, options)
-      const data = await res.json()
+      const response = await fetch(url, options) // 서버에서 데이터 가져올때까지 기다린다
+      //   console.log(response) // 기다렸다가 다 가지고 오면 실행
+
+      const data = await response.json() //await를 지정하는 이유: fetch는 비동기적으로 실행되므로 서버에서 request 해오는 딜레이 시간 중에 실행 된다.
       const results = data.results
-
       const container = document.querySelector('main .container')
-      let rowHtml = ''
+      let rowsHtml = '' //모든 row를 담을 문자열 변수
 
-      for (let i = 0; i < results.length; i += 1) {
+      //5번 반복 => row를 5개 만들어준다
+      for (let i = 0; i < results.length; i += 2) {
          let rowHtml = '<div class="row">'
 
-         for (let j = 0; j < 19; j++) {
+         //4번 반복 => col을 4개 만들어준다
+         for (let j = 0; j < 2; j++) {
             const index = i + j
-            if (index >= results.length) break
+            if (index >= results.length) break //results 배열을 벗어나면 중단
 
             const tv = results[index]
 
             rowHtml += `
-             <div class="col-sm-3 p-3">
-                     <div class="card">
+                <div class="col-sm-3 p-3">
+                    <div class="card">
                         <a href="./detail.html?tv_id=${tv.id}">
-                           <img src="https://image.tmdb.org/t/p/w500${tv.poster_path}" class="card-img-top poster" alt="${tv.title}" />
+                           <img src="https://image.tmdb.org/t/p/w500${tv.poster_path}" class="card-img-top poster" alt="${tv.name}" />
                         </a>
                         <div class="card-body">
-                           <p class="card-text title">${tv.title}</p>
+                        <p class="card-text name">${tv.name}</p>
+                        <p class="card-text average">${Number(tv.vote_average) === 0 ? '미반영' : ' 평점' + tv.vote_average.toFixed(1)}</p>
+                        <p class="card-text overview ellipsis">${tv.overview}</p>
                         </div>
-                     </div>
+                    </div>
                 </div>
-            `
+                `
          }
+
          rowHtml += '</div>'
-         rowHtml += rowHtml
+         rowsHtml += rowHtml
       }
-      container.innerHTML = rowHtml
+
+      container.innerHTML = rowsHtml
    } catch (error) {
-      console.log('에러발생', error)
+      console.log('에러 발생:', error)
    }
 }
 
-getPlayingTv(tvPopularUrl)
+getPlayingtvs(url)
--- 08_웹사이트만들기/tmdb/js/tv_detail.js
+++ 08_웹사이트만들기/tmdb/js/tv_detail.js
@@ -5,25 +5,48 @@ const options = {
       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4OGJjYWQ5NDExM2I4NWQ0MGUzMjgwZDRiYzJiZjJjMiIsIm5iZiI6MTc0ODM5NDAxNS4zODIwMDAyLCJzdWIiOiI2ODM2NjAxZmEzMmM2OWU2MWMwNTVlYTQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.ZnD1ETNpj68Ym3ZX49CNPqPz_uG_0n8rN82o-LIN1d8',
    },
 }
+const urlParams = new URLSearchParams(window.location.search)
 
+const tvId = urlParams.get('tv_id')
 
-const tvDetailUrl = 'https://api.themoviedb.org/3/tv/14981?language=ko-KR'
+const tvDetailUrl = `https://api.themoviedb.org/3/tv/${tvId}language=ko-KR`
+const mainContainer = document.querySelector('main .container')
 
 const getDetailtv = async (tvDetailUrl) => {
    try {
       const response = await fetch(tvDetailUrl, options)
 
       const data = await response.json()
-      console.log('success data:', data)
+      // console.log('success data:', data)
+
+      const imgSrc = `https://image.tmdb.org/t/p/w300${data.poster_path}`
+
+      const rowHtml = `
+               <div class="row">
+                  <div class="col-sm-3">
+                     <img src="${imgSrc}" class="poster-detail" alt="${data.title}"/>
+                  </div>
+                  <div class="col-sm-9 tv-detail">
+                     <h2>${data.title}</h2>
+                     <ul class="tv-info">
+                        <li>개봉일 ${data.release_date}</li>
+                        <li>
+                           ${data.genres.map((genre) => {
+                              return genre.name
+                           })}
+                        </li>
+                        <li>${data.runtime}분</li>
+                     </ul>
+                     <p>평점 ${Number(data.vote_average) === 0 ? '미반영' : data.vote_average.toFixed(1)}</p>
+                     <p>${data.overview}</p>
+                  </div>
+               </div>`
+
+      // console.log(rowHtml)
+      mainContainer.innerHTML += rowHtml
    } catch (error) {
       console.error('에러 발생:', error)
    }
 }
 
-getDetailMovie(tvDetailUrl)
-
-
-
-      
-
-   
+getDetailtv(tvDetailUrl)
